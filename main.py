37a7e6eed4f93f5050274b022d9a87a95e5ee736
ADITHI-SATISH <adithi.satish@betsol.com>
2022-09-19 12:08:00 +0530

    initial commit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import json
 from pathlib import Path
+from starlette.background import BackgroundTask
 import os
 from fastapi import FastAPI, Form,File,UploadFile
 from fastapi.responses import FileResponse
@@ -15,11 +17,15 @@ doc = DocxTemplate("./form2revised.docx")
 my_app = FastAPI()
 
 @my_app.post("/getInformation")
-async def handle_form(name:str=Form(),fathers_name_or_husbands_name:str=Form(),signature: UploadFile = File(...),dob:str=Form(),gender:str=Form(),maritalstatus:str=Form(),pf_number:str=Form(),address:str=Form(),name_and_address_of_nominee1:str=Form(None),nominee1_relationship:str=Form(None),dob1:str=Form(None),totalamt_or_share1:str=Form(None),if_nominee1_is_a_minor_mention_guardian_name_and_address:str=Form(None),name_and_address_of_nominee2:str=Form(None),nominee2_relationship:str=Form(None),dob2:str=Form(None),totalamt_or_share2:str=Form(None),if_nominee2_is_a_minor_mention_guardian_name_and_address:str=Form(None),name_and_address_of_nominee3:str=Form(None),nominee3_relationship:str=Form(None),dob3:str=Form(None),totalamt_or_share3:str=Form(None),if_nominee3_is_a_minor_mention_guardian_name_and_address:str=Form(None),name_and_address_of_nominee4:str=Form(None),nominee4_relationship:str=Form(None),dob4:str=Form(None),totalamt_or_share4:str=Form(None),if_nominee4_is_a_minor_mention_guardian_name_and_address:str=Form(None),name_address_of_the_family_member1:str=Form(None),epsdob1:str=Form(None),relationship1_with_member:str=Form(None),name_address_of_the_family_member2:str=Form(None),epsdob2:str=Form(None),name_address_of_the_family_member3:str=Form(None),relationship2_with_member:str=Form(None),name_address_of_the_family_member4:str=Form(None),epsdob3:str=Form(None),epsdob4:str=Form(None),relationship3_with_member:str=Form(None),relationship4_with_member:str=Form(None),name_and_address_of_nominee1_eps:str=Form(None),name_and_address_of_nominee2_eps:str=Form(None),name_and_address_of_nominee3_eps:str=Form(None),dob1epsnominee:str=Form(None),dob2epsnominee:str=Form(None),dob3epsnominee:str=Form(None),relation1:str=Form(None),relation2:str=Form(None),relation3:str=Form(None)):
+async def handle_form(name:str=Form(),fathers_name_or_husbands_name:str=Form(),signature: UploadFile = File(...),dob:str=Form(),gender:str=Form(),maritalstatus:str=Form(),pf_number:str=Form(),address:str=Form(),epf_nominee_details:str=Form(),eps_member_details:str=Form(),eps_nominee:str=Form()):
+ if(os.path.isfile(f"./form2.docx")):
+  os.remove(f"./form2.docx") 
  file_location = f"{signature.filename}"    
  with open(file_location, "wb+") as file_object:
         file_object.write(signature.file.read())
-
+ epf=json.loads(epf_nominee_details)
+ eps_members=json.loads(eps_member_details)
+ eps=json.loads(eps_nominee)
  context = {
          "name": name,
          "fathers_name_or_husbands_name":fathers_name_or_husbands_name,
@@ -28,50 +34,24 @@ async def handle_form(name:str=Form(),fathers_name_or_husbands_name:str=Form(),s
          "maritalstatus":maritalstatus,
          "pf_number": pf_number,
          "address":textwrap.fill(address,width=240),
-         "name_and_address_of_nominee1":name_and_address_of_nominee1,
-         "nominee1_relationship": nominee1_relationship,
-         "dob1": dob1,
-         "totalamt_or_share1":totalamt_or_share1,
-         "if_nominee1_is_a_minor_mention_guardian_name_and_address":if_nominee1_is_a_minor_mention_guardian_name_and_address,
-         "name_and_address_of_nominee2": name_and_address_of_nominee2,
-         "nominee2_relationship":nominee2_relationship,
-         "dob2":dob2,
-         "totalamt_or_share2": totalamt_or_share2,
-         "if_nominee2_is_a_minor_mention_guardian_name_and_address":if_nominee2_is_a_minor_mention_guardian_name_and_address,
-         "name_and_address_of_nominee3":name_and_address_of_nominee3,
-         "nominee3_relationship":nominee3_relationship,
-         "dob3":dob3,
-         "totalamt_or_share3":totalamt_or_share3,
-         "if_nominee3_is_a_minor_mention_guardian_name_and_address":if_nominee3_is_a_minor_mention_guardian_name_and_address,
-         "name_and_address_of_nominee4": name_and_address_of_nominee4,
-         "nominee4_relationship": nominee4_relationship,
-         "dob4": dob4,
-         "totalamt_or_share4": totalamt_or_share4,
-         "if_nominee4_is_a_minor_mention_guardian_name_and_address": if_nominee4_is_a_minor_mention_guardian_name_and_address,
-         "name_address_of_the_family_member1":name_address_of_the_family_member1,
-         "epsdob1":epsdob1,
-         "relationship1_with_member":relationship1_with_member,
-         "name_address_of_the_family_member2":name_address_of_the_family_member2,
-         "epsdob2":epsdob2,
-         "relationship2_with_member":relationship2_with_member,
-         "name_address_of_the_family_member3":name_address_of_the_family_member3,
-         "epsdob3":epsdob3,
-         "relationship3_with_member":relationship3_with_member,
-         "name_address_of_the_family_member4":name_address_of_the_family_member4,
-         "epsdob4":epsdob4,
-         "relationship4_with_member":relationship4_with_member,
-         "name_and_address_of_nominee1_eps":name_and_address_of_nominee1_eps,
-         "dob1epsnominee":dob1epsnominee,
-         "relation1":relation1,
-         "name_and_address_of_nominee2_eps":name_and_address_of_nominee2_eps,
-         "dob2epsnominee":dob2epsnominee,
-         "relation2":relation2,
-         "name_and_address_of_nominee3_eps":name_and_address_of_nominee3_eps,
-         "dob3epsnominee":dob3epsnominee,
-         "relation3":relation3,
          "TODAY": today.strftime("%d-%m-%Y"), 
          "TODAY_IN_ONE_WEEK": today_in_one_week.strftime("%d-%m-%Y"),
      }
+ for i in range(len(epf)):
+         context[f"name_and_address_of_nominee{i+1}"]=epf[i]["name_and_address_of_nominee"]
+         context[f"nominee{i+1}_relationship"]=epf[i]["nominee_relationship"]
+         context[f"dob{i+1}"]=epf[i]["dob"]
+         context[f"totalamt_or_share{i+1}"]=epf[i]["totalamt_or_share"]
+         context[f"if_nominee{i+1}_is_a_minor_mention_guardian_name_and_address"]=epf[i]["if_nominee_is_a_minor_mention_guardian_name_and_address"]
+ for i in range(len(eps_members)):
+         context[f"name_address_of_the_family_member{i+1}"]=eps_members[i]["name_address_of_the_family_member"]
+         context[f"epsdob{i+1}"]=eps_members[i]["epsdob"]
+         context[f"relationship{i+1}_with_member"]=eps_members[i]["relationship_with_member"]
+ for i in range(len(epf)):
+         context[f"name_and_address_of_nominee{i+1}_eps"]= eps[i]["name_and_address_of_nominee_eps"]
+         context[f"dob{i+1}epsnominee"]= eps[i]["dobepsnominee"]
+         context[f"relation{i+1}"]= eps[i]["relation"]
+      
 
  doc.render(context)
  
@@ -93,10 +73,8 @@ async def handle_form(name:str=Form(),fathers_name_or_husbands_name:str=Form(),s
        
 
        
- doc.save(f"./{context['name']}-form2.docx")
- document_path = Path(__file__).parent /f"./{context['name']}-form2.docx"
- os.remove(file_location)
- return FileResponse(path=document_path,filename=f"./{context['name']}-form2.docx")
-       
-
+ doc.save(f"./form2.docx")
+ document_path = Path(__file__).parent /f"./form2.docx"
+ os.remove(file_location)   
+ return FileResponse(path=document_path,filename=f"./form2.docx")
 
